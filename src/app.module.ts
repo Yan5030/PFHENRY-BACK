c5534459641f209b965c4cb696a58e2925a1613c
jhon darwin llanos narvaez <jhondarwinllanosnarvaez@gmail.com>
2025-01-08 20:40:27 -0500

    Componente user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DishModule } from './dish/dish.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DishModule],
   controllers: [AppController],
   providers: [AppService],
 })
