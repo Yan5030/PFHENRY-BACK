b029c7eda9ac4f6f3157821c60e9229ab9f84f93
ALVAREZ YANINA <140518462+Yan5030@users.noreply.github.com>
2025-01-13 23:27:59 -0300

    cambio en el middleware de date

--- src/middlewares/date-validation/date-validation.middleware.ts
+++ src/middlewares/date-validation/date-validation.middleware.ts
@@ -6,7 +6,7 @@ import { NextFunction, Request, Response } from 'express';
 export class DateValidationMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const { date, time } = req.body;
-
+    if(date && time){
     // Validar formato de fecha
     if (!dayjs(date, 'YYYY-MM-DD', true).isValid()) {
       throw new BadRequestException('La fecha no está en formato YYYY-MM-DD');
@@ -36,5 +36,10 @@ export class DateValidationMiddleware implements NestMiddleware {
 
     // Continuar con la solicitud si todo es válido
     next();
+  }else{
+    next();
   }
+  }
+
+  
 }
