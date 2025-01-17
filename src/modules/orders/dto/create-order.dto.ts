5881b67c5e0263d78ef25b9921453c6670c1905a
ALVAREZ YANINA <140518462+Yan5030@users.noreply.github.com>
2025-01-17 09:57:58 -0300

    elimicacion de archivos de prueba, agrego cambios de orders

--- src/modules/orders/dto/create-order.dto.ts
+++ src/modules/orders/dto/create-order.dto.ts
@@ -1,1 +1,9 @@
-export class CreateOrderDto {}
+import { PaymentMethod } from "src/enum/payment-method.enum";
+import { MenuItem } from "src/modules/menuItems/entities/menuItems.entities";
+import { CreateOrderDetailDto } from "../../orders-details/dto/create-order-details.dto";
+
+export class CreateOrderDto {
+idUser : string; // order
+paymentMethod : PaymentMethod; //order
+MenuItems: CreateOrderDetailDto [] //orderDet
+}
--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrderDetail } from './entities/Order-detail.entity';
+import { OrderDetail } from '../orders-details/entities/order-detail.entity'; 
 import { MenuItem } from '../menuItems/entities/menuItems.entities';
+import { Order } from './entities/order.entity';
+import { UsersService } from '../users/users.service';
+import { OrderDetailsService } from '../orders-details/order-details.service';
+import { User } from '../users/entities/user.entity';
+import { MenuItemService } from '../menuItems/menuItem.service';
+import { Category } from '../categories/entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderDetail, MenuItem])],
-  controllers: [OrdersController],
-  providers: [OrdersService],
+  imports: [TypeOrmModule.forFeature([OrderDetail, MenuItem,Order,User,Category])],
+controllers: [OrdersController],
+  providers: [OrdersService,UsersService,OrderDetailsService,MenuItemService],
 })
 export class OrdersModule {}
