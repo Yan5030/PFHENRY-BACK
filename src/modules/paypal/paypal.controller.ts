529517ab38d2b358a0998eb809dfc3f2398b5ab4
nahudavila12 <nahu.davila@gmail.com>
2025-02-03 13:06:25 -0300

    Listo Paypal, Nodemailer

--- src/modules/paypal/paypal.controller.ts
+++ src/modules/paypal/paypal.controller.ts
@@ -1,18 +1,25 @@
-import { Controller, Post, Body, Param } from "@nestjs/common";
+import { Controller, Post, Param, BadRequestException } from "@nestjs/common";
 import { PayPalService } from "./paypal.service";
 
-
-@Controller('paypal')
+@Controller("paypal")
 export class PayPalController {
   constructor(private readonly payPalService: PayPalService) {}
 
-  @Post('create-order/:orderId')
+  @Post("create-order/:orderId")
   async createOrder(@Param("orderId") orderId: string) {
-    return this.payPalService.createOrder(orderId);
+    try {
+      return await this.payPalService.createOrder(orderId);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 
-  @Post('orders/:orderId/capture')
-  async captureOrder(@Param('orderId') orderId: string) {
-    return this.payPalService.captureOrder(orderId);
+  @Post("capture-order/:orderId")
+  async captureOrder(@Param("orderId") orderId: string) {
+    try {
+      return await this.payPalService.captureOrder(orderId);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 }
