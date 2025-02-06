304ced7b4b827556f0ee32e5acf7c6e8ed575558
nahudavila12 <nahu.davila@gmail.com>
2025-02-06 16:12:04 -0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: ['https://pf-henry-front-k0x5rb2jt-nahudavila12s-projects.vercel.app/'], 
+    origin: ['https://pf-henry-front-8o6560lpj-nahudavila12s-projects.vercel.app/'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH"],
     allowedHeaders: 'Content-type, Authorization',
     credentials: true, 
