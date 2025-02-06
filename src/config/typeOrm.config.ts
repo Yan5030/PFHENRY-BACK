2966d2a425c1947d4fce1238ec2b6042eb9e962a
Agusmalugani <agusmalugani97@gmail.com>
2025-02-05 22:05:45 -0300

    config db

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -16,10 +16,10 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   entities: [__dirname + '/../**/*.entity.{js,ts}'], // entities : [ 'dist/**/*.entity{.ts,.js}' ]
   migrations: ['dist/migrations/*{.js,.ts}'],
   synchronize: true,
-  //dropSchema: true,
+  dropSchema: true,
   //logging: true,
    ssl: { 
      rejectUnauthorized: false, // Render requiere esto para conexiones seguras
-   },}
- // ssl: false,
+ },}
+//  ssl: false,
 //};
